b56214afa9148cf1cd3b8ff7f754f8b98f97631b
stefluu <luu.stefanie@gmail.com>
2017-06-13 22:37:46 -0700

    Completed HW 7

--- README.md
+++ README.md
@@ -4,9 +4,15 @@
 ---
 1. Feynman Writing Prompts - Write out explanations of the following concepts like you are explaining it to a 12 year old.  Doing this will help you quickly discover any holes in your understanding.  Ask your questions on Slack.
 		
-	* Array
-	* Object
-	* Method
+	* Array: 
+	An array is a data type in Javascript that is a collection of information. Some real-life collection examples are collecting stickers, tsum tsums, catching pokemon on Pokemon Go.  These are all collections of similar items. We can also collect things that aren't similar. For example, maybe we want to collect action hero-themed items. This might contain all sorts of items: clothing, toys, stickers, comics. Arrays in Javascript are like these collections, except with data and values. They can contain all the same data types, (like if you collected only stickers), or different data types, (like if you collected action hero-themed items that aren't all the same type of items). Arrays can also contain functions in its collection.  A function is code that is remembered and action-provoking. A function is a remembered "action" code in an array, which is similar to a remembered physical action in real life. Similar to how your body has muscle memory for actions, arrays have functions for actions. We store these functions and their "actions" in an array like we store physical actions in our muscle memory.  Our muscle memory might store many actions, just like an array can store many functions.
+	
+	* Object: 
+	An object is a named collection of values, properties, and methods. A real-life example of an object can be compared to a pet. A pet has many characteristics. For example, a pet can be many types of animals. "Type" is a characteristic of the pet.  "Dog" can be the kind of "type" of pet. Therefore, if we were to write this in Javascript, "pet" would be the object name, "type" would be a property name or key, and "dog" would be the value assigned to the property of "type".
+	
+	
+	* Method:
+	A function within an object is known as a method.  Let's use the pet example from before. A real life example of an object method is if the pet knows a trick. Knowing a trick is a characteristic of the pet, and there is an action associated with the trick. Therefore in Javascript, "trick" would be the property name, and the action of the trick is the "method".
 
 
 3. Fork and clone this repo.  When you need to commit use the following commands.
--- exercises.js
+++ exercises.js
@@ -3,6 +3,15 @@
 function getBiggest(x, y) {
   //x and y are integers.  Return the larger integer
   //if they are the same return either one
+  if (x > y){
+	return x;
+  }
+  else if (y > x){
+	return y;
+  }
+  else{
+	return x || y;
+  }
 }
 
 function greeting(language) {
@@ -11,15 +20,41 @@ function greeting(language) {
   //language: 'English' -> 'Hello!'
   //language: 'Spanish' -> 'Hola!'
   //if language is undefined return 'Hello!'
+  if(language === 'German'){
+	return 'Guten Tag!';
+  }
+  else if(language === 'English'){
+	return 'Hello!';
+  }
+  
+  else if(language === 'Spanish'){
+	return 'Hola!';
+  }
+  else{
+	return 'Hello!';
+  }
+  
 }
 
 function isTenOrFive(num) {
   //return true if num is 10 or 5
   //otherwise return false
+if(num === 10 || num === 5){
+	return true;
+}
+else{
+	return false;
+}
 }
 
 function isInRange(num) {
   //return true if num is less than 50 and greater than 20
+  if(num < 50 && num >20){
+	return true;
+  }
+  else {
+	return false;
+  }
 }
 
 function isInteger(num) {
@@ -29,6 +64,13 @@ function isInteger(num) {
   //-10 -> true
   //otherwise return false
   //hint: you can solve this using Math.floor
+  if(Math.floor(num) === num){
+	return true;
+  }
+  else{
+	return false;
+  }
+  
 }
 
 function fizzBuzz(num) {
@@ -36,7 +78,20 @@ function fizzBuzz(num) {
   //if num is divisible by 5 return 'buzz'
   //if num is divisible by 3 & 5 return 'fizzbuzz'
   //otherwise return num
+  if(num%15===0){
+	return('fizzbuzz');
+	}	
+  else if(num%3 ===0){
+	return('fizz');
+	}
+  else if(num%5 === 0){
+	return('buzz');
+	}
+  else{
+	return num;
+	}
 }
+
 
 function isPrime(num) {
   //return true if num is prime.
@@ -44,10 +99,20 @@ function isPrime(num) {
   //hint: a prime number is only evenly divisible by itself and 1
   //hint2: you can solve this using a for loop
   //note: 0 and 1 are NOT considered prime numbers
+  if(num === 0 || num === 1){
+	return false;
+  }
+  for(var i = 2; i < num; i++){
+	if(num%i === 0){  
+	return false;
+	}
+  }
+  return true;
 }
 
 function returnFirst(arr) {
   //return the first item from the array
+  return arr[0];
 }
 
 function returnLast(arr) {
@@ -55,27 +120,36 @@ function returnLast(arr) {
   // version 1
   // return arr[arr.length - 1];
   // version 2
+  return arr[arr.length-1];
 }
 
 function getArrayLength(arr) {
   //return the length of the array
+  return arr.length;
 }
 
 function incrementByOne(arr) {
   //arr is an array of integers
   //increase each integer by one
   //return the array
+  return arr.map(function(num){
+	return num + 1;
+  });
 }
 
 function addItemToArray(arr, item) {
   //add the item to the end of the array
   //return the array
+  arr.push(item);
+  return arr;
 }
 
 function addItemToFront(arr, item) {
   //add the item to the front of the array
   //return the array
   //hint: use the array method .unshift
+ arr.unshift(item);
+ return arr;
 }
 
 function wordsToSentence(words) {
@@ -83,26 +157,51 @@ function wordsToSentence(words) {
   //return a string that is all of the words concatenated together
   //spaces need to be between each word
   //example: ['Hello', 'world!'] -> 'Hello world!'
+  return words.join(' ');
 }
 
 function contains(arr, item) {
   //check to see if item is inside of arr
   //return true if it is, otherwise return false
+  if(arr.includes(item) === true){ //tests if something is included in the array
+	return true;
+  }
+  else{
+	return false;
+  }
 }
 
 function addNumbers(numbers) {
   //numbers is an array of integers.
   //add all of the integers and return the value
+  var total = 0;
+  for(var i = 0; i <numbers.length; i++){
+	total += numbers[i];
+	}
+	return total; 
 }
 
 function averageTestScore(testScores) {
   //testScores is an array.  Iterate over testScores and compute the average.
   //return the average
+  var total = 0;
+  var arrElements = testScores.length;
+  for(var i = 0; i < testScores.length; i++){
+    total += testScores[i];
+  }
+  return total/arrElements;
 }
 
 function largestNumber(numbers) {
   //numbers is an array of integers
   //return the largest integer
+  var greatest = 0;
+  for(var i = 0; i < numbers.length; i++){
+	if(numbers[i] > greatest){
+	greatest = numbers[i];
+	}
+  }
+  return greatest;
 }
 
 function makeCat(name, age) {
@@ -110,61 +209,104 @@ function makeCat(name, age) {
   //add an age property to the object with the value set to the age argument
   //add a method called meow that returns the string 'Meow!'
   //return the object
+  var NewCat = function (name, age){
+    this.name = name;
+    this.age = age;
+  };
+  
+  var cat = new NewCat(name, age);
+  
+  cat['meow'] = function(){return 'Meow!';};
+  
+  return cat;
+  
 }
 
 function addProperty(object, property) {
   //add the property to the object with a value of null
   //return the object
   //note: the property name is NOT 'property'.  The name is the value of the argument called property (a string)
+  object[property]=null;
+  return object;
 }
 
 function invokeMethod(object, method) {
   //method is a string that contains the name of a method on the object
   //invoke this method
   //nothing needs to be returned
+   object[method]();
 }
 
 function multiplyMysteryNumberByFive(mysteryNumberObject) {
   //mysteryNumberObject has a property called mysteryNumber
   //multiply the mysteryNumber property by 5 and return the product
+  return mysteryNumberObject['mysteryNumber'] *5;
 }
 
 function deleteProperty(object, property) {
   //remove the property from the object
   //return the object
+  delete object[property];
+  return object;
 }
 
 function newUser(name, email, password) {
   //create a new object with properties matching the arguments passed in.
   //return the new object
+  var newObj = {
+	name : name,
+	email : email,
+	password : password
+  };
+ return newObj;
 }
 
 function hasEmail(user) {
   //return true if the user has a value for the property 'email'
   //otherwise return false
+  if(user.email){
+	return true;
+  }
+	return false;
 }
 
 function hasProperty(object, property) {
   //return true if the object has the value of the property argument
   //property is a string
   //otherwise return false
+  if(property in object === true){
+	return true;
+  }
+  else{
+	return false;
+  }
 }
 
 function verifyPassword(user, password) {
   //check to see if the provided password matches the password property on the user object
   //return true if they match
   //otherwise return false
+  if(user['password'] === password){
+	return true;
+  }
+  else{
+	return false;
+  }
 }
 
 function updatePassword(user, newPassword) {
   //replace the existing password on the user object with the value of newPassword
   //return the object
+  user.password = newPassword;
+  return user;
 }
 
 function addFriend(user, newFriend) {
   //user has a property called friends that is an array
   //add newFriend to the end of the friends array
   //return the user object
+  user['friends'].push(newFriend);
+  return user;
 }
 
 function setUsersToPremium(users) {
@@ -172,6 +314,11 @@ function setUsersToPremium(users) {
   //each user object has the property 'isPremium'
   //set each user's isPremium property to true
   //return the users array
+  
+  for(var i = 0; i < users.length; i++){
+	users[i].isPremium = true;
+  }
+  return users;
 }
 
 function sumUserPostLikes(user) {
@@ -180,6 +327,12 @@ function sumUserPostLikes(user) {
   //each post object has an integer property called 'likes'
   //sum together the likes from all the post objects
   //return the sum
+  var sum = 0;
+  var postsArr = user['posts'];
+  for (var i = 0; i < postsArr.length; i++){
+	sum += postsArr[i].likes;
+  }  
+  return sum;
 }
 
 function addCalculateDiscountPriceMethod(storeItem) {
@@ -190,6 +343,11 @@ function addCalculateDiscountPriceMethod(storeItem) {
   //price -> 20
   //discountPercentage -> .2
   //discountPrice = 20 - (20 * .2)
+  storeItem['calculateDiscountPrice'] = function(){
+  return storeItem['price'] - (storeItem['price'] * storeItem['discountPercentage']);
+  };
+  
+  return storeItem;
 }
 
 
